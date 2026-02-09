e03615999ff6c4317a715db1b41125c0f139405e
Jaifin-aloor <aloorjaifin@gmail.com>
2026-02-09 21:30:44 +0530

    created controller

---
+++ 1 persistence and databases/notes-api-postgres/src/controller/noteController.ts
@@ -0,0 +1,83 @@
+import type { Request, Response } from "express";
+import { Note } from "../model/Note.js"
+import { NoteRepo } from "../repository/noteRepo.js";
+import { NOTE_NAME } from '../model/Note.js';
+
+class NoteController{
+
+    async create(req: Request, res: Response){
+        try{
+            const new_note = new Note();
+            new_note.name = req.body.name;
+            new_note.description = req.body.description;
+
+            await new NoteRepo().save(new_note);
+
+            res.status(201).json({
+                status: "Note created sucessfully",
+                message: "Successfully created note."
+            });
+        }catch(error){
+            res.status(500).json({error: "internal server error."});
+        }
+    }
+
+    async delete(req: Request, res: Response){
+        try{
+            let id = parseInt(String(req.params["id"]));
+            await new NoteRepo().delete(id);
+            res.status(200).json({
+                status: "deleted note successfully.",
+                message: "Successfully deleted note."
+            });
+        }catch(error){
+            res.status(500).json({error: "internal server error."});
+        }
+    }
+
+    async findAll(req: Request, res: Response){
+        try{
+            const new_note = await new NoteRepo().retriveAll();
+            res.status(200).json({
+                status: "OK!",
+                message: "Succesfully fetched all note data!",
+                data: new_note
+            });
+        }catch(error){
+            res.status(500).json({error: "internal server error."});
+        }
+    }
+
+    async findById(req: Request, res:Response){
+        try{
+            let id = parseInt(String(req.params["id"]));
+            const new_note = await new NoteRepo().retriveById(id);
+            res.status(200).json({
+                status: "OK!",
+                message: "Succesfully fetched note data by id!",
+                data: new_note
+            });
+        }catch(error){
+            res.status(500).json({error: "internal server error."});
+        }
+    }
+
+    async update(req: Request, res: Response){
+        try{
+            let id = parseInt(String(req.params["id"]));
+            const new_note = new Note();
+            new_note.id = id;
+            new_note.name = req.body.name;
+            new_note.description = req.body.description;
+
+            await new NoteRepo().update(new_note);
+
+            res.status(200).json({
+                status: "OK!",
+                message: "Succesfully updated note data by id!",
+            });
+        }catch(error) {
+            res.status(500).json({error: "internal server error."});
+        }
+    }
+}
