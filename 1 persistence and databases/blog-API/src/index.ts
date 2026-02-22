51935d2831c17ce213ee0cf7984d31733f6fc139
Jaifin-aloor <aloorjaifin@gmail.com>
2026-02-22 23:50:45 +0530

    Completed the project.

--- 1 persistence and databases/blog-API/src/index.ts
+++ 1 persistence and databases/blog-API/src/index.ts
@@ -2,15 +2,51 @@ import express from "express";
 import { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectDB, sequelize } from "./database";
-import User from "./models/user.model";
-import Post from "./models/post.model";
+
+import "./models/user.model";
+import "./models/post.model";
+import "./models/comment.model";
+
+import userRoutes from "./routes/user.routes";
+import postRoutes from "./routes/post.routes";
+import commentRoutes from "./routes/comment.routes";
 
 dotenv.config();
 
-const testConnection = async () => {
-    await connectDB();
-    await sequelize.sync();
-    console.log("All tables created successfully.");
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
+
+// Health check
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Blog API is running."
+    });
+});
+
+// Start server
+const startServer = async (): Promise<void> => {
+    try {
+        // Connect to DB
+        await connectDB();
+        // Sync all models
+        await sequelize.sync();
+        console.log("Database connected successfully.");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.error("Failed to start server: ", error);
+        process.exit(1);
+    }
 };
 
-testConnection();
+startServer();
--- 1 persistence and databases/blog-API/src/routes/comment.routes.ts
+++ 1 persistence and databases/blog-API/src/routes/comment.routes.ts
@@ -1,8 +1,82 @@
 import { Router, Request, Response } from "express";
-import Comment from "../models/comment.model";
+import Comment from '../models/comment.model';
 import Post from "../models/post.model";
 import { validateCreateComment } from "../utils/validate";
+
+const handlingServerErrors = (e: any, message: string, res: Response): void => {
+    console.error(e);
+    res.status(500).json({
+        message: message
+    });
+};
 
 const router = Router();
 
 // CREATE comment
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        if (!validateCreateComment(req, res)) return;
+        const { content, postId } = req.body;
+        // To ensure post exists
+        const post = await Post.findByPk(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found."
+            });
+        }
+        const comment = await Comment.create({
+                content,
+                postId
+        });
+        res.status(201).json(comment);
+    } catch (error) {
+        handlingServerErrors(error, "Failed to create comment.", res)
+    }
+});
+
+// GET all comments
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const comments = await Comment.findAll();
+        res.status(200).json(comments);
+    } catch (error) {
+        handlingServerErrors(error, "Failed to fetch comments.", res)
+    }
+});
+
+// GET comment by id
+router.get("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const comment = await Comment.findByPk(id);
+        if (!comment) {
+            return res.status(404).json({
+                message: "Comment not found."
+            });
+        }
+        res.status(200).json(comment);
+    } catch (error) {
+        handlingServerErrors(error, "Failed to fetch comment.", res)
+    }
+});
+
+// DELETE comment
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const comment = await Comment.findByPk(id);
+        if (!comment) {
+            return res.status(404).json({
+                message: "Comment not found."
+            });
+        }
+        await comment.destroy();
+        res.status(200).json({
+            message: "Comment deleted successfully."
+        })
+    } catch (error) {
+        handlingServerErrors(error, "Failed to delete comment.", res)
+    }
+});
+
+export default router;
