7dc5930b95473f09235c3c23732b73d69076500b
Jaifin-aloor <aloorjaifin@gmail.com>
2026-03-15 22:04:47 +0530

    jwt and password hashing utility and user model completed.

---
+++ 2 authentication and real-world apis/auth-service/src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+interface JwtPayload {
+    userId: number;
+    role: string;
+}
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN as SignOptions["expiresIn"] || "1h";
+
+export function generateToken(payload: JwtPayload): string {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+}
+
+export function verifyToken(token: string): JwtPayload {
+    return jwt.verify(token, JWT_SECRET) as JwtPayload;
+}
